939b31a576b90ab4a16d02c595101d4127cd39ea
Matias Yanez <matias.yanez@usach.cl>
2022-11-05 14:55:52 -0300

    inicializacion microservicio empleado

---
+++ src/test/java/proyecto/mingeso/microserviceempleado/MicroserviceEmpleadoApplicationTests.java
@@ -0,0 +1,13 @@
+package proyecto.mingeso.microserviceempleado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceEmpleadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
