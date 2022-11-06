847f8083e6ab66040b6065c91328bb89faae5faa
Matias Yanez <matias.yanez@usach.cl>
2022-11-06 16:43:59 -0300

    Implementacion de eureka

--- src/main/java/proyecto/mingeso/microserviceempleado/MicroserviceEmpleadoApplication.java
+++ src/main/java/proyecto/mingeso/microserviceempleado/MicroserviceEmpleadoApplication.java
@@ -2,8 +2,10 @@ package proyecto.mingeso.microserviceempleado;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MicroserviceEmpleadoApplication {
 
 	public static void main(String[] args) {
