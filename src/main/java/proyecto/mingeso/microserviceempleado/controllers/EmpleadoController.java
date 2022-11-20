9b365756ff002487dc1030b2cf269bd3205a1ae8
Matias Yanez <matias.yanez@usach.cl>
2022-11-20 19:59:11 -0300

    crossOrigin

--- src/main/java/proyecto/mingeso/microserviceempleado/controllers/EmpleadoController.java
+++ src/main/java/proyecto/mingeso/microserviceempleado/controllers/EmpleadoController.java
@@ -2,10 +2,7 @@ package proyecto.mingeso.microserviceempleado.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import proyecto.mingeso.microserviceempleado.entities.EmpleadoEntity;
 import proyecto.mingeso.microserviceempleado.services.EmpleadoService;
 
@@ -13,6 +10,7 @@ import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/empleado")
+@CrossOrigin(origins = "http://localhost:3000")
 public class EmpleadoController {
 
     @Autowired
